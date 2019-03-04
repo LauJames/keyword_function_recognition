d06dd549da7f286ff46f275182ccdf405d2bba5b
LauJames <LauJames_work@163.com>
2019-03-04 10:47:13 +0800

    + add key phrase extract task program

---
+++ glove/key_phrase_extract.py
@@ -0,0 +1,13 @@
+#! /user/bin/evn python
+# -*- coding:utf8 -*-
+
+"""
+
+@Author   : Lau James
+@Contact  : LauJames2017@whu.edu.cn
+@Project  : keyword_function_recognition 
+@File     : key_phrase_extract.py
+@Time     : 2019/3/4 10:46
+@Software : PyCharm
+@Copyright: "Copyright (c) 2018 Lau James. All Rights Reserved"
+"""
